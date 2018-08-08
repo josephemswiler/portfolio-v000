e9cd6f6d4a8f0cf86e3ddeb433ab78a35307c377
josephemswiler <josephemswiler@gmail.com>
2018-08-08 14:39:01 -0500

    Init react, redux

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const PORT = process.env.PORT || 3000
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('client/src'))
 
 // require('./controllers/porfolio-controller.js')(app)
 
